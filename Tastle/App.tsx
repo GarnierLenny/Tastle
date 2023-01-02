e28fb186851d9bb9e95b20ccff9c4931d6c06762
Lenny Garnier <lenny.garnier@epitech.eu>
2023-01-02 10:40:16 +0400

    fix TastleApp not finding user location - switched from js to ts

--- Tastle/App.tsx
+++ Tastle/App.tsx
@@ -3,8 +3,21 @@ import { Button, StyleSheet, Text, ToastAndroid, TouchableOpacity, View } from '
 import MapView from 'react-native-maps';
 import * as Location from 'expo-location';
 import { useState, useEffect } from 'react';
+import * as Permissions from 'expo-permissions';
 
 global.centered = 0;
+
+async function askForLocationPermission() {
+  const { status } = await Permissions.askAsync(Permissions.LOCATION);
+  if (status !== 'granted') {
+    console.log('Location permission not granted');
+  }
+}
+
+async function getUserLocation() {
+  const location = await Location.getCurrentPositionAsync({});
+  console.log(location);
+}
 
 function App() {
 
@@ -17,11 +30,13 @@ function App() {
       );
     }
 
+  askForLocationPermission();
+  getUserLocation();
   const [region, setRegion] = useState({
       latitude: 0,
       longitude: 0,
-      latitudeDelta: 0.00922,
-      longitudeDelta: 0.00421,
+      latitudeDelta: 50.00922,
+      longitudeDelta: 50.00421,
   });
 
   const changeRegion = (newRegion) => {
@@ -30,28 +45,15 @@ function App() {
   
   return (
     <View style={styles.container}>
-     <MapView
-      style={styles.map}
-      onUserLocationChange={(event) => {
-        if (centered == 1)
-          return;
-        centered = 1;
-        console.log("recentered!");
-        setRegion({
-            latitude: event.nativeEvent.coordinate.latitude,
-            longitude: event.nativeEvent.coordinate.longitude,
-            latitudeDelta: 0.00922,
-            longitudeDelta: 0.00421,
-          });
-        }
-      } 
-      region={region}
-      showsUserLocation={true}
-      followsUserLocation={true}
-      showsMyLocationButton={true}
-      myLocationButtonAnchor={{ x: 50, y: 50 }}
-      mapPadding={{ top: 0, right: 0, bottom: 0, left: 0 }}
+      <Text style={styles.text}></Text>
+      <MapView
+        style={styles.map}
+        showsUserLocation={true}
+        followsUserLocation={true}
+        showsMyLocationButton={true}
+        toolbarEnabled={true}
       />
+      <Text style={styles.text}></Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -62,8 +64,7 @@ export default App;
 const styles = StyleSheet.create({
   map: {
     width: '100%',
-    height: '100%',
-    flex: 1,
+    height: '85%'
   },
   container: {
     flex: 1,
